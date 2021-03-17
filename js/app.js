30d1d1eb71f9d45b3a41e1e6639dd3751848d98c
Tony Lai <tonytonglai2000@gmail.com>
2021-03-16 23:27:39 -0400

    yah

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const RPS = ["Rock", "Paper", "Scissors"]; // will be used to draw from here...
 const STATEMENTS = ["You lose :(", "You win!", "Tie game."];
+let runningSum = 0;
 
 function computerPlay() {
     let selection = Math.round((Math.random()*2)); // should ideally yield some number between 0-2...
@@ -11,7 +12,7 @@ function winnerChecker(playerChoice, computerSelection) {
     // rock = 0, paper = 1, scissors = 2. 
     let playerIndex = RPS.indexOf(playerChoice);
     let computerIndex = RPS.indexOf(computerSelection);
-    
+
     if (playerIndex == 0 && computerIndex == 2){
         // if player picks rock, comp picks scissors...
         return 1;
@@ -25,20 +26,48 @@ function winnerChecker(playerChoice, computerSelection) {
         return 2;
     }
 
-    console.log(playerChoice > computerIndex);
-    return +(playerChoice > computerIndex); // unary operator '+' used to convert bool into number.
+    return +(playerIndex > computerIndex); // unary operator '+' used to convert bool into number.
 }
 // I'll make this the wrapper function...
+
+function winConverter(value) {
+    if (value == 2) {
+        return 0.5;
+    }
+
+    return value;
+}
+
 function play(){
     let playerChoice = prompt("Enter a choice between 'Rock', 'Paper', 'Scissors': ");
     let computerSelection = computerPlay();
-    console.log(computerSelection);
+    console.log(`Computer Selection is ${computerSelection}`);
+    let total = +winnerChecker(playerChoice, computerSelection); // 0 is win, 1 is loss, 2 is tie...
+    runningSum += winConverter(total);
+    console.log(`Running total is ${runningSum}`);
+
     return STATEMENTS[winnerChecker(playerChoice, computerSelection)];
 
 
 }
 
-console.log(play());
+function playToFive(runningSum){
+    // runningSum = 0;
+    let totalRuns = 0;
+    while (totalRuns < 5) {
+        play();
+        totalRuns++;
+    }
 
+    console.log(`Your running sum was ${runningSum}`);
+    if (runningSum > 2.5) {
+        return STATEMENTS[1];
+    } else if (runningSum == 2.5) {
+        return STATEMENTS[2];
+    } else {
+        return STATEMENTS[0];
+    }
 
+}
 
+console.log(playToFive());
